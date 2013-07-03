dae4c6f39261cb6b520876b89b39d858dc41a86e
Daniel Halperin <dhalperi@cs.washington.edu>
2013-07-02 20:08:14 -0700

    SampleJaxRsApplication: instantiate a `BeanConfig` in the main program
    Credit @fehguy: see discussion at
    https://github.com/wordnik/swagger-core/issues/226
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/main/java/com/wordnik/swagger/sample/jersey/SampleJaxRsApplication.java
+++ src/main/java/com/wordnik/swagger/sample/jersey/SampleJaxRsApplication.java
@@ -1,6 +1,7 @@
 package com.wordnik.swagger.sample.jersey;
 
 import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.wordnik.swagger.jaxrs.config.BeanConfig;
 
 public final class SampleJaxRsApplication extends PackagesResourceConfig {
 
@@ -11,6 +12,11 @@ public final class SampleJaxRsApplication extends PackagesResourceConfig {
 
 	public static void main(String[] args) throws Exception {
 		final int port = 8002;
+
+		BeanConfig config = new BeanConfig();
+		config.setResourcePackage("com.wordnik.swagger.sample.resource");
+		config.setVersion("1.0.0");
+		config.setBasePath("http://localhost:8002/");
 
 		SampleJaxRsWebserver server = new SampleJaxRsWebserver(port);
 		server.start();
