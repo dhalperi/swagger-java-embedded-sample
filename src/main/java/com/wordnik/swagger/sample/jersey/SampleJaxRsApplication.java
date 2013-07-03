9436369e5fbed0111644052a6ad90ef2a653ce40
Daniel Halperin <dhalperi@cs.washington.edu>
2013-07-02 23:13:08 -0700

    SampleJaxRsApplication: add CORS filter
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/main/java/com/wordnik/swagger/sample/jersey/SampleJaxRsApplication.java
+++ src/main/java/com/wordnik/swagger/sample/jersey/SampleJaxRsApplication.java
@@ -1,6 +1,9 @@
 package com.wordnik.swagger.sample.jersey;
 
 import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
 import com.wordnik.swagger.jaxrs.config.BeanConfig;
 
 public final class SampleJaxRsApplication extends PackagesResourceConfig {
@@ -8,6 +11,7 @@ public final class SampleJaxRsApplication extends PackagesResourceConfig {
 	public SampleJaxRsApplication() {
 		super("com.wordnik.swagger.sample.resource",
 				"com.wordnik.swagger.jaxrs.listing");
+		getContainerResponseFilters().add(new SampleCrossOriginFilter());
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -16,11 +20,24 @@ public final class SampleJaxRsApplication extends PackagesResourceConfig {
 		BeanConfig config = new BeanConfig();
 		config.setResourcePackage("com.wordnik.swagger.sample.resource");
 		config.setVersion("1.0.0");
-		config.setBasePath("http://localhost:8002/");
+		config.setBasePath("http://localhost:8002/api");
 
 		SampleJaxRsWebserver server = new SampleJaxRsWebserver(port);
 		server.start();
 		System.in.read();
 		server.stop();
+	}
+
+	private class SampleCrossOriginFilter implements ContainerResponseFilter {
+		@Override
+		public ContainerResponse filter(ContainerRequest request,
+				ContainerResponse response) {
+			response.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+			response.getHttpHeaders().add("Access-Control-Allow-Methods",
+					"GET, POST, DELETE, PUT");
+			response.getHttpHeaders().add("Access-Control-Allow-Headers",
+					"Content-Type");
+			return response;
+		}
 	}
 }
